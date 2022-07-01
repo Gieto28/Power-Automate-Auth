2e4cef8d248acad9505aa87c81fe9e83175d7862
gieto28 <28miguelangelo@gmail.com>
2022-07-01 10:10:54 +0100

    fixed res.json

--- src/actions/createTask.ts
+++ src/actions/createTask.ts
@@ -4,9 +4,7 @@ const action = async (req: Request, res: Response) => {
   try {
     console.log(req.body);
 
-    const body = req.body;
-
-    return res.status(200).json({ body });
+    return res.status(200).json({ success: "sucess" });
   } catch (e) {
     return res.status(400).json({ success: "false" });
   }
